1868e97d090722cbc7a5eb1d5d1b5689866d56a1
Christen Ford <christen0ford@gmail.com>
2020-11-10 02:16:34 -0500

    update analyzer so it includes analysis for sub files

---
+++ CodeMetrics/Analysis/CSharpCodeAnalyzer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Analysis
+{
+    public class CSharpCodeAnalyzer
+    {
+        private CSharpSyntaxTree SyntaxTree { get; }
+        private List<CSharpClass> SubClasses = new List<CSharpClass>();
+
+        public CSharpCodeAnalyzer(string filepath)
+        {
+            SyntaxTree = (CSharpSyntaxTree)CSharpSyntaxTree.ParseText(File.ReadAllText(filepath));
+            IEnumerable<ClassDeclarationSyntax> classes = SyntaxTree.GetRoot()
+                .DescendantNodes()
+                .OfType<ClassDeclarationSyntax>().ToList();
+            List<ClassDeclarationSyntax> classSyntax = new List<ClassDeclarationSyntax>(classes);
+            foreach (var cls in classSyntax)
+            {
+                SubClasses.Add(new CSharpClass((CSharpSyntaxTree)cls.SyntaxTree));
+            }
+        }
+
+        public IEnumerable<CSharpClass> IterSubClasses()
+        {
+            foreach (var cls in SubClasses)
+            {
+                yield return cls;
+            }
+        }
+    }
+}
